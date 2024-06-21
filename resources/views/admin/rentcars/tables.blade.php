076c00df1587a535736014e844049178d4b6bfda
intan appkey <intan.appkey@gmail.com>
2024-06-21 11:05:26 +0800

    clear code

--- resources/views/admin/rentcars/tables.blade.php
+++ resources/views/admin/rentcars/tables.blade.php
@@ -284,8 +284,6 @@
                   <script>
                     document.write(new Date().getFullYear());
                   </script>
-                  , made with ❤️ by
-                  <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
                 </div>
               </div>
             </footer>
